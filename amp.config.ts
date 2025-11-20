5e30ce6719eb914cfb6cb148e40c94e28cd1bcbc
Chris Whited <cmwhited1990@gmail.com>
2025-11-20 20:06:17 -0300

    fixes

--- amp.config.ts
+++ amp.config.ts
@@ -3,7 +3,16 @@ import { defineDataset, eventTables } from "@edgeandnode/amp"
 import { abi } from "./app/src/lib/abi.ts"
 
 export default defineDataset(() => ({
+  namespace: "amp_demo", // replace this with a namespace of your choosing that will be a grouping of your datasets
   name: "counter",
+  // readme: `
+  // # amp_demo/counter.
+  //
+  // provide additional, helpful details about your dataset, its purpose and usage.
+  // `,
+  // description: "High-level description of your dataset and its use-case/purpose",
+  // keywords: ["DeFi"],
+  // sources: ["0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"],
   network: process.env.VITE_AMP_NETWORK || "anvil",
   dependencies: {
     rpc: process.env.VITE_AMP_RPC_DATASET || "_/anvil@0.0.1",
